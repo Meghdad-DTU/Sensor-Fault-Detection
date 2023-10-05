82033b579f365efccabe98e936ba3142772cfd89
meghdad <mehdizadeh.iust@gmail.com>
2023-10-05 13:04:03 -0400

    completeing training pipeline by configuring aws credintaals on aws cli

--- src/sensorFaultDetection/pipeline/training_pipeline.py
+++ src/sensorFaultDetection/pipeline/training_pipeline.py
@@ -1,4 +1,5 @@
 import sys
+from datetime import datetime
 from sensorFaultDetection.logger import logging
 from sensorFaultDetection.exception import CustomException
 from sensorFaultDetection.pipeline.stage_01_data_ingestion import DataIngestionPipeline
@@ -6,56 +7,96 @@ from sensorFaultDetection.pipeline.stage_02_data_validation import DataValidatio
 from sensorFaultDetection.pipeline.stage_03_data_transformation import DataTransformationPipeline
 from sensorFaultDetection.pipeline.stage_04_model_trainer import ModelTrainerPipeline
 from sensorFaultDetection.pipeline.stage_05_model_evaluation import ModelEvaluationPipeline
+from sensorFaultDetection.config.configuration import ConfigurationManager
+from sensorFaultDetection.entity.config_entity import TrainingPipelineConfig
+from sensorFaultDetection.cloud_storage.s3_syncer import S3Sync
 
 
 
 class TrainingPipeline:
     is_pipeline_running= False
-    def __init__(self):
-        pass
+    def __init__(self, config: TrainingPipelineConfig):
+        self.config = config        
 
+    def sync_artifact_dir_to_s3(self):
+        try:
+            now = datetime.now
+            timestamp = format(now().strftime('%Y-%m-%d_%H:%M:%S'))
+            aws_bucket_url = f's3://{self.config.training_bucket_name}/artifacts/{timestamp}'
+            S3Sync.sync_folder_to_s3(folder= self.config.artifacts_dir, aws_bucket_url= aws_bucket_url)
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_bucket_url = f's3://{self.config.training_bucket_name}/{self.config.saved_model_dir}'
+            S3Sync.sync_folder_to_s3(folder= self.config.saved_model_dir, aws_bucket_url= aws_bucket_url)
+        except Exception as e:
+            raise CustomException(e, sys)
+    
     def run_pipeline(self):
         try: 
             TrainingPipeline.is_pipeline_running = True       
             STAGE_NAME = "Data Ingestion Stage"
 
             logging.info(f'>>>>>>> {STAGE_NAME} started <<<<<<<<')
-            obj = DataIngestionPipeline()
-            obj.main()
+            dataIngestion_obj = DataIngestionPipeline()
+            dataIngestion_obj.main()
             logging.info(f'>>>>>>> {STAGE_NAME} completed <<<<<<<<') 
 
             STAGE_NAME = "Data Validation Stage"
 
             logging.info(f'>>>>>>> {STAGE_NAME} started <<<<<<<<')
-            obj = DataValidationPipeline()
-            obj.main()
+            dataValidation_obj = DataValidationPipeline()
+            dataValidation_obj.main()
             logging.info(f'>>>>>>> {STAGE_NAME} completed <<<<<<<<')
 
             STAGE_NAME = "Data Transformation Stage"
 
             logging.info(f'>>>>>>> {STAGE_NAME} started <<<<<<<<')
-            obj = DataTransformationPipeline()
-            obj.main()
+            dataTransformation_obj = DataTransformationPipeline()
+            dataTransformation_obj.main()
             logging.info(f'>>>>>>> {STAGE_NAME} completed <<<<<<<<')
 
             STAGE_NAME = "Model Training Stage"
 
             logging.info(f'>>>>>>> {STAGE_NAME} started <<<<<<<<')
-            obj = ModelTrainerPipeline()
-            obj.main()
-            logging.info(f'>>>>>>> {STAGE_NAME} completed <<<<<<<<')                
-
+            modelTrainer_obj = ModelTrainerPipeline()
+            modelTrainer_obj.main()
+            logging.info(f'>>>>>>> {STAGE_NAME} completed <<<<<<<<')
 
             STAGE_NAME = "Model Evaluation Stage"
 
             logging.info(f'>>>>>>> {STAGE_NAME} started <<<<<<<<')
-            obj = ModelEvaluationPipeline()
-            obj.main()
+            modelEvaluation_obj = ModelEvaluationPipeline()
+            modelEvaluation_obj.main()
             logging.info(f'>>>>>>> {STAGE_NAME} completed <<<<<<<<')
             
-            TrainingPipeline.is_pipeline_running = True    
+            if not modelEvaluation_obj.is_model_accepted:
+                self.sync_artifact_dir_to_s3()
+                logging.info('The artifacts are uploaded in aws s3!')
+            else:
+                self.sync_artifact_dir_to_s3()
+                self.sync_saved_model_dir_to_s3()
+                logging.info('The artifacts and model are uploaded in aws s3!')           
+            
+            
+            TrainingPipeline.is_pipeline_running = False           
         
         except Exception as e:
+            TrainingPipeline.is_pipeline_running = False 
             raise CustomException(e, sys)  
         
 
+STAGE_NAME = 'Training Pipeline'
+if __name__ == "__main__":    
+    try:        
+        logging.info(f'>>>>>>> stage {STAGE_NAME} started <<<<<<<<')
+        config = ConfigurationManager()
+        training_pipeline_config = config.get_training_pipeline_config()
+        training_pipeline = TrainingPipeline(config=training_pipeline_config)
+        training_pipeline.run_pipeline()    
+        logging.info(f'>>>>>>> stage {STAGE_NAME} completed <<<<<<<<')
+    
+    except Exception as e:
+        raise CustomException(e, sys)
